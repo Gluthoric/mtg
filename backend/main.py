ee15dca28244b999c8b2c37b1ff7dfe9de08f93f
Your Name <you@example.com>
2024-09-22 15:15:49 -0500

    Setting up flask-migrate

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_migrate import Migrate
 from config import Config
 from database import db
 from routes import register_routes
 from routes.set_routes import set_routes
-from sqlalchemy import inspect, text
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +12,7 @@ def create_app():
 
     # Initialize extensions
     db.init_app(app)
+    migrate = Migrate(app, db)
     CORS(app)
 
     # Register routes
@@ -20,40 +21,6 @@ def create_app():
 
     return app
 
-def alter_tables(app):
-    with app.app_context():
-        try:
-            # Alter kiosk table
-            db.session.execute(text("ALTER TABLE kiosk ALTER COLUMN id SET DEFAULT nextval('kiosk_id_seq'::regclass)"))
-            db.session.execute(text("ALTER TABLE kiosk ALTER COLUMN id SET NOT NULL"))
-
-            # Alter collections table
-            db.session.execute(text("ALTER TABLE collections ALTER COLUMN id SET DEFAULT nextval('collections_id_seq'::regclass)"))
-            db.session.execute(text("ALTER TABLE collections ALTER COLUMN id SET NOT NULL"))
-
-            db.session.commit()
-            print("Tables altered successfully.")
-        except Exception as e:
-            db.session.rollback()
-            print(f"Error altering tables: {str(e)}")
-
-def initialize_database(app):
-    with app.app_context():
-        inspector = inspect(db.engine)
-        if not inspector.has_table("cards"):
-            print("Creating database tables...")
-            db.create_all()
-            print("Database tables created.")
-        else:
-            print("Database tables already exist.")
-
-        # Alter tables to add auto-increment
-        alter_tables(app)
-
 if __name__ == '__main__':
     app = create_app()
-
-    # Initialize database (create tables if they don't exist and alter them)
-    initialize_database(app)
-
     app.run(debug=True)
