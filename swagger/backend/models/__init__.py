ad8caedb4a5544d1a634fa271a3bf9116492b052
Your Name <you@example.com>
2024-10-05 14:07:29 -0500

    Remove Swagger folder

--- swagger/backend/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .card import Card
-from .set import Set
-from .set_collection_count import SetCollectionCount
-
-# This file ensures that all models are imported when the models package is imported
